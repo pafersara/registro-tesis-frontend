672a34c96ce47f24c101e25c84f2a8a772aec6f3
Paul Fernando Sacaca Ramirez <paul.sacaca@unmsm.edu.pe>
2019-09-24 10:54:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Update from './Components/Update';
+
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/update" component={Update} />
+          <Redirect component={Login} to="/login" />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
